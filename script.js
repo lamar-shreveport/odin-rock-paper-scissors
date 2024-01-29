cf4ebf68de7b37c42e430061c6e02b8ea8a0f45d
lamar-shreveport <127351320+lamar-shreveport@users.noreply.github.com>
2024-01-29 10:23:11 -0500

    Implement scoring system, restart button

--- script.js
+++ script.js
@@ -4,10 +4,14 @@
 // 2 = Paper
 // 3 = Scissors
 
-
 let computerChoice = 0;
 let playerChoice = 0;
 let score = 0;
+let div = document.querySelector('div');
+let restartBtn = document.querySelector('#restart');
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
 
 function getComputerChoice() {
     // Chooses a random number: 1, 2, or 3
@@ -15,35 +19,25 @@ function getComputerChoice() {
     computerChoice = randomNumber;
 }
 
-function getPlayerChoice() {
-    let playerInput = prompt("Rock, Paper, or Scissors?");
-    playerInput.toLowerCase();
-    if (playerInput == "rock") {
-        playerChoice = 1;
-    }
-    else if (playerInput == "paper") {
-        playerChoice = 2;
-    }
-    else if (playerInput == "scissors") {
-        playerChoice = 3;
-    }
-
-}
-
 function playRound() {
     getComputerChoice();
-    getPlayerChoice();
+
+    // getPlayerChoice(
+    let p = document.createElement("p");
 
     // Player chooses Rock
     if (playerChoice == 1) {
         if (computerChoice == 1) {
-            console.log("It's a tie! Rock vs Rock!");
+            p.append("It's a tie! Rock vs Rock!");
+            div.appendChild(p);
         }
         else if (computerChoice == 2) {
-            console.log("You lose! Paper covers Rock!");
+            p.append("You lose! Paper covers Rock!");
+            div.appendChild(p);
         }
         else if (computerChoice == 3) {
-            console.log("You win! Rock smashes Scissors!");
+            p.append("You win! Rock smashes Scissors!");
+            div.appendChild(p);
             score += 1;
         }
     }
@@ -51,41 +45,84 @@ function playRound() {
     // Player chooses Paper
     if (playerChoice == 2) {
         if (computerChoice == 1) {
-            console.log("You win! Paper covers Rock!");
+            p.append("You win! Paper covers Rock!");
+            div.appendChild(p);
             score += 1;
         }
         else if (computerChoice == 2) {
-            console.log("It's a tie! Paper vs Paper!");
+            p.append("It's a tie! Paper vs Paper!");
+            div.appendChild(p);
         }
         else if (computerChoice == 3) {
-            console.log("You lose! Scissors cut Paper!!");
+            p.append("You lose! Scissors cut Paper!");
+            div.appendChild(p);
         }
     }
 
     // Player chooses Scissors
     if (playerChoice == 3) {
         if (computerChoice == 1) {
-            console.log("You lose! Rock smashes Scissors!");
+            p.append("You lose! Rock smashes Scissors!");
+            div.appendChild(p);
         }
         else if (computerChoice == 2) {
-            console.log("You win! Scissors cut Paper!");
+            p.append("You win! Scissors cut Paper!");
+            div.appendChild(p);
             score += 1;
         }
         else if (computerChoice == 3) {
-            console.log("It's a tie! Scissors vs Scissors!");
+            p.append("It's a tie! Scissors vs Scissors!");
+            div.appendChild(p);
         }
     }
+
+}
+
+// Empties play history
+
+function clearHistory() {
+    document.querySelector('#play-history').innerHTML = "";
+    restartBtn.style.visibility = 'hidden';
+    rockBtn.style.visibility = 'visible';
+    paperBtn.style.visibility = 'visible';
+    scissorsBtn.style.visibility = 'visible';
 }
 
 // Plays the game!
+
 function game() {
-    score = 0;
     playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    console.log(score);
+
+    // Find the last paragraph so we can add the score
+    // Cursed?
+    let lastP = document.querySelector('p:last-child');
+    lastP.append(` Score = ${score}\n`);
+
+    // Win @ 5. If click another button, clear and start new game
+    if (score === 5) {
+        let h1 = document.createElement('H1');
+        h1.textContent = "You win!";
+        lastP.append(h1);
+        score = 0;
+
+        restartBtn.style.visibility = 'visible';
+        rockBtn.style.visibility = 'hidden';
+        paperBtn.style.visibility = 'hidden';
+        scissorsBtn.style.visibility = 'hidden';
+    }
 }
 
-game();
+const button = document.querySelectorAll('button');
+
+// Need to cycle through buttons for event listener
+button.forEach(button => button.addEventListener('click', () => {
+    if(button.textContent == 'Rock') playerChoice = 1;
+    else if (button.textContent == 'Paper') playerChoice = 2;
+    else if (button.textContent == 'Scissors') playerChoice = 3;
+    game();
+
+}));
+
+restartBtn.addEventListener('click', ()=>{
+    clearHistory();
+});
